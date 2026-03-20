c4c72b97652e6904f0d498162646c58c8e7de888
Sandeep Singh <ssandeep8696@gmail.com>
2026-03-20 23:00:35 +0530

    added manifest

---
+++ app/manifest.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from "next";
+
+import { siteConfig } from "@/lib/site-config";
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: siteConfig.name,
+        short_name: siteConfig.shortName,
+        description: siteConfig.description,
+        start_url: "/",
+        display: "browser",
+        background_color: "#ffffff",
+        theme_color: "#fafafa",
+    };
+}
